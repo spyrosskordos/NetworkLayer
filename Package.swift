d2f9025a02c5824b955d10fa2cd8abd7dc0d1cae
Spyrosskordos <spyrosskordos@gmail.com>
2023-09-26 22:41:04 +0300

    http client

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkLayer",
+    platforms: [.iOS(.v12), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NetworkLayer",
-            targets: ["NetworkLayer"]),
+            targets: ["NetworkLayer"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
