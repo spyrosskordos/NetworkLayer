2adeb1a3c0eae99c9e9a55ab42c3104d096e2bb5
Spyrosskordos <spyrosskordos@gmail.com>
2023-09-26 22:22:19 +0300

    Request method enum

---
+++ Sources/NetworkLayer/RequestMethod/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  
+//
+//  Created by Sko on 26/9/23.
+//
+
+import Foundation
+
+public enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
