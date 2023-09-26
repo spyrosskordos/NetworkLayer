410abe972a28a4232654443a585e270673b7e578
Spyrosskordos <spyrosskordos@gmail.com>
2023-09-26 22:38:01 +0300

    request error

---
+++ Sources/NetworkLayer/RequestError/RequestError.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sko on 26/9/23.
+//
+
+import Foundation
+
+public enum RequestError: Error {
+    case decode
+    case invalidURL
+    case noResponse
+    case unauthorized
+    case unexpectedStatusCode
+    case unknown
+    
+    var customMessage: String {
+        switch self {
+        case .decode:
+            return "Decode error"
+        case .unauthorized:
+            return "Session expired"
+        default:
+            return "Unknown error"
+        }
+    }
+}
