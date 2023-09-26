de92082c6bd4c4bd838805a6b72308cfc38e4206
Spyrosskordos <spyrosskordos@gmail.com>
2023-09-26 22:22:36 +0300

    endpoint protocol

---
+++ Sources/NetworkLayer/Protocols/Endpoint.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sko on 26/9/23.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+}
